6d7a3b6b25a14945f0a1f831bd5703f0e6a05d8e
tabetant <antoine.tabet@mail.utoronto.ca>
2025-05-30 13:09:17 +0300

    POST route implemented

--- src/app/api/applications/route.ts
+++ src/app/api/applications/route.ts
@@ -9,7 +9,25 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
+    if (!request.body) return;
+    const body = await request.json();
+    const jobTitle = body.jobTitle as string;
+    const dateApplied = new Date(body.dateApplied).toISOString().slice(0, 10); // 'YYYY-MM-DD'
+    const company = body.company as string;
+    const status = body.status as "applied" | "interview" | "offer" | "rejected";
+    const notes = body.notes as string;
 
+    await db.insert(applications).values({
+        jobTitle,
+        company,
+        dateApplied,
+        status,
+        notes,
+    })
+    return new NextResponse(JSON.stringify({ message: 'Application created' }), {
+        status: 201,
+        headers: { 'Content-Type': 'application/json' },
+    });
 }
 
 export async function PATCH(request: Request) {
