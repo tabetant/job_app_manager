53071993dc0852cd3ebc19c0797de7b5869e6811
tabetant <antoine.tabet@mail.utoronto.ca>
2025-06-03 14:45:47 +0300

    modified file structure for authentication

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import JobForm from '../ui/JobForm'
+
+export default function App() {
+  return (
+    <>
+      <h1 className='text-center text-4xl font-bold my-2'>Job Application Manager</h1>
+      <JobForm />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import JobForm from './ui/JobForm'
-
+import Link from 'next/link'
 export default function App() {
-  return (
-    <>
-      <h1 className='text-center text-4xl font-bold my-2'>Job Application Manager</h1>
-      <JobForm />
-    </>
-  );
+    return (
+        <>
+            <h1>Welcome to Job Applications Manager</h1>
+            <Link href='/signup' className='hover:text-purple-500'>Sign Up</Link>
+            <Link href='/login' className='hover:text-purple-500'>Log In</Link>
+            <Link href='/dashboard' className='hover:text-purple-500'>Dashboard</Link>
+        </>
+    )
 }
